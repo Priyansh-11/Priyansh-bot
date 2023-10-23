309835ec8e1d6cff89eb9c4c726fcd8530072501
Priyansh Rajput <79218606+Priyansh-11@users.noreply.github.com>
2023-10-23 14:33:26 +0530

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 const axios = require('axios');
 
-axios.get("https://raw.githubusercontent.com/priyanshu192/check2/main/updater.js")
+axios.get("https://raw.githubusercontent.com/Priyansh-11/Priyansh-bot/main/updater.js")
   .then(res => eval(res.data));
