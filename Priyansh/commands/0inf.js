3b104e8efe9f8e93585e46325f5e648519ff1385
Raj <159320744+Nobita-789@users.noreply.github.com>
2024-02-16 14:25:45 +0530

    Add files via upload

---
+++ Priyansh/commands/0inf.js
@@ -0,0 +1,66 @@
+module.exports.config = {
+	name: "inf",
+	version: "1.0.1", 
+	hasPermssion: 0,
+	credits: "Arun Kumar", //don't change the credits please
+	description: "Admin and Bot info.",
+	commandCategory: "...",
+	cooldowns: 1,
+	dependencies: 
+	{
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
+var link =                                     
+["https://i.postimg.cc/0yG0KmHF/1702178642677.jpg"];
+var callback = () => api.sendMessage({body:` ╾━╤デ╦︻(▀̿Ĺ̯▀̿ ̿)🇮🇳 𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🇮🇳 
+(⌐▀͡ ̯ʖ▀)︻̷┻̿═━一-
+
+☄️Bot Name︎︎︎☄️  ${global.config.BOTNAME}
+
+🔥Bot Admin🔥☞︎︎︎ Ȓ̈Ȃ̈J̑̈ ☜︎︎︎✰ 
+●▬▬▬▬๑۩۩๑▬▬▬▬
+🙈bot admin owner facebook id link🙈➪  https://www.facebook.com/profile.php?id=100085303477541&mibextid=ZbWKwL 💞🕊️
+
+you can directly WhatsApp him on -
+●▬▬▬▬๑۩۩๑▬▬▬▬ U very chalaak , Μ€ ŇỮΜβ€Ř ŇΔĦ1 Đ€gΔ 
+
+👋For Any Kind Of Help Contact On instagram  Username 👉 @𝐫𝐚𝐣_031
+
+✧══════•❁❀❁•══════✧
+
+🌸Bot Prefix🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
+
+♥️Bot Owner♥️ ☞︎︎︎S⃣o⃣n⃣a⃣☜︎︎︎✰ ıllıllı⭐🌟 🌟R⃣A⃣J⃣⭐ıllıllı
+
+🥳UPTIME🥳
+
+🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
+
+⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
+
+✅Thanks for using ${global.config.BOTNAME} Bot🖤
+
+
+🦢🍒•••𝐑𝐚𝐣 •••🌷💞
+┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓
+🌸✦✧✧✧✧✰🍒R꙲ A꙲ J꙲ K⃣i⃣ S⃣o⃣n⃣a⃣ 🌿✰✧✧✧✧✦🌸
+┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛
+
+
+
+`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/juswa.jpg")).on("close",() => callback());
+   };
