d685da3f24542759dab0468cc090153fb29892e4
Raj <159320744+Nobita-789@users.noreply.github.com>
2024-03-16 23:11:23 +0530

    Update admin.js

--- Priyansh/commands/admin.js
+++ Priyansh/commands/admin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "god": {
-            const god = ["100017985245260"];
+            const god = ["100085303477541"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
