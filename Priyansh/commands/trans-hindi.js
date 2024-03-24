d949ed2e3c03bbf4a0328fb2ebece98d4c752fa6
Raj <159320744+Nobita-789@users.noreply.github.com>
2024-03-24 21:11:31 +0530

    Add files via upload

---
+++ Priyansh/commands/trans-hindi.js
@@ -0,0 +1,39 @@
+module.exports.config = {
+	name: "hindi",
+	version: "1.0.1",
+	hasPermssion: 0,
+	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	description: "Text translation",
+	commandCategory: "media",
+	usages: "[Text]",
+	cooldowns: 5,
+	dependencies: {
+		"request":  ""
+	}
+};
+
+module.exports.run = async ({ api, event, args }) => {
+	const request = global.nodemodule["request"];
+	var content = args.join(" ");
+	if (content.length == 0 && event.type != "message_reply") return global.utils.throwError(this.config.name, event.threadID,event.messageID);
+	var translateThis = content.slice(0, content.indexOf(" ->"));
+	var lang = content.substring(content.indexOf(" -> ") + 4);
+	if (event.type == "message_reply") {
+		translateThis = event.messageReply.body
+		if (content.indexOf("-> ") !== -1) lang = content.substring(content.indexOf("-> ") + 3);
+		else lang = global.config.language;
+	}
+	else if (content.indexOf(" -> ") == -1) {
+		translateThis = content.slice(0, content.length)
+		lang = global.config.language;
+	}
+  
+	return request(encodeURI(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=hi&dt=t&q=${translateThis}`), (err, response, body) => {
+		if (err) return api.sendMessage("An error has occurred!", event.threadID, event.messageID);
+		var retrieve = JSON.parse(body);
+		var text = '';
+		retrieve[0].forEach(item => (item[0]) ? text += item[0] : '');
+		var fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0]
+		api.sendMessage(` ${text}\n - 🍂🍂 ${fromLang} to 𝐇𝐢𝐧𝐝𝐢🍂🍂`, event.threadID, event.messageID);
+	});
+                                    }
