f46f2bc911049d660ef7d33438099812a6fe9043
Raj <159320744+Nobita-789@users.noreply.github.com>
2024-04-03 17:45:06 +0530

    Update meme.js

--- Priyansh/commands/meme.js
+++ Priyansh/commands/meme.js
@@ -29,7 +29,7 @@ var min = Math.floor(Math.random() * 2);
       if(money < 0) api.sendMessage("You need 200$ to see the photo!",event.threadID,event.messageID)
           else {
    Currencies.setData(event.senderID, options = {money: money -0})
-   var callback = () => api.sendMessage({body:`MADE BY PRIYANSH: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID); 
+   var callback = () => api.sendMessage({body:`MADE BY RAJ: ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)] + (max - min))).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
      }
    };
