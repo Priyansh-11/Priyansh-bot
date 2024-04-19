25b6061c7ec1e0d89a52cc2a7ae7987a44fb3dc8
Raj <159320744+Nobita-789@users.noreply.github.com>
2024-04-19 18:30:44 +0530

    Update report.js

--- Priyansh/commands/report.js
+++ Priyansh/commands/report.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 				attachment: fs.createReadStream(__dirname + `/noprefix/report.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🍺", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🙂", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
