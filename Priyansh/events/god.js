6ddb95cbcfd5d15630a0ff900e176bdd785b211a
amitkumarrajpu <157116791+amitkumarrajpu@users.noreply.github.com>
2024-08-10 17:28:22 +0530

    Update god.js

--- Priyansh/events/god.js
+++ Priyansh/events/god.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "100037743553265";
+  var god = "61561421229589";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
