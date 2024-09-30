9a3f1b29edb4cfe924037135ad59b8488a35e0f9
Priyansh Rajput <79218606+Priyansh-11@users.noreply.github.com>
2024-09-30 18:19:16 +0530

    Update Priyansh.js

--- Priyansh.js
+++ Priyansh.js
@@ -1,6 +1,7 @@
 const { readdirSync, readFileSync, writeFileSync, existsSync, unlinkSync, rm } = require("fs-extra");
 const { join, resolve } = require("path");
-const { execSync } = require('child_process'); 
+const { execSync } = require('child_process');
+const chalk = require('chalk');
 const logger = require("./utils/log.js");
 const login = require("fca-priyansh");
 const axios = require("axios");
