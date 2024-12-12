be4b61f6fa2b3550ac17c84225ef41a911092795
Priyansh Rajput <79218606+Priyansh-11@users.noreply.github.com>
2024-12-12 22:13:15 +0530

    Create shell.js

---
+++ Priyansh/commands/shell.js
@@ -0,0 +1,28 @@
+module.exports.config = {
+	name: "shell",
+	version: "7.3.1",
+	hasPermssion: 2,
+	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	description: "running shell",
+	commandCategory: "System",
+	usages: "[shell]",
+	cooldowns: 0,
+	dependencies: {
+		"child_process": ""
+	}
+};
+module.exports.run = async function({ api, event, args, Threads, Users, Currencies, models }) {    
+const { exec } = require("child_process");
+let text = args.join(" ")
+exec(`${text}`, (error, stdout, stderr) => {
+    if (error) {
+        api.sendMessage(`error: \n${error.message}`, event.threadID, event.messageID);
+        return;
+    }
+    if (stderr) {
+        api.sendMessage(`stderr:\n ${stderr}`, event.threadID, event.messageID);
+        return;
+    }
+    api.sendMessage(`stdout:\n ${stdout}`, event.threadID, event.messageID);
+});
+}
