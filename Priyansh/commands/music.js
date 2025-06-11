6272930617a3cb6645140e4d187579204e5f1f1e
Priyansh Rajput <79218606+Priyansh-11@users.noreply.github.com>
2025-06-11 09:16:26 +0530

    Update music.js

--- Priyansh/commands/music.js
+++ Priyansh/commands/music.js
@@ -50,7 +50,7 @@ module.exports = {
       const videoId = topResult.videoId;
 
       const apiKey = "priyansh-here";
-      const apiUrl = `https://priyansh-ai.onrender.com/youtube?id=${videoId}&type=${type}&apikey=${apiKey}`;
+      const apiUrl = `https://priyanshuapi.xyz/youtube?id=${videoId}&type=${type}&apikey=${apiKey}`;
 
       api.setMessageReaction("⌛", event.messageID, () => {}, true);
 
